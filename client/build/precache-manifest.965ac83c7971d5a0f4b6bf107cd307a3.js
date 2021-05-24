f67204295fb1ec1fd42b5865ba8f5d64de75039a
sunny <sunny@xecurify.com>
2021-05-24 15:24:32 +0530

    updated gitignore

---
+++ client/build/precache-manifest.965ac83c7971d5a0f4b6bf107cd307a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8f61f35ed774c0a8dce1540a73b4575",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f6597c216aace53719e",
+    "url": "/static/css/main.ec05f99e.chunk.css"
+  },
+  {
+    "revision": "5e3250b25c62aa04c4ae",
+    "url": "/static/js/2.77f9f702.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.77f9f702.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f6597c216aace53719e",
+    "url": "/static/js/main.5bbf123e.chunk.js"
+  },
+  {
+    "revision": "5387050906b8ed53cb42",
+    "url": "/static/js/runtime-main.ec2d9410.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
