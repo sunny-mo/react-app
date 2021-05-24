5fdd3308ae3a0162ec76b40d3b1e07b57dec9b02
sunny <sunny@xecurify.com>
2021-05-24 15:34:42 +0530

    master lost

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const MoJwt = require('./miniorange-jwt-connector.node');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 15000;
+const port = process.env.PORT || 5000;
 // let jwtBuilder = new MoJwt.jwtBuilder;
 
 app.use(express.static(path.join(__dirname, './build')));
